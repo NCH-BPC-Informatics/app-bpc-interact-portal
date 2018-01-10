c198c876380b77ddd77052c14764f2468b748ef3
Eric Kramer <cosmocracy@yahoo.com>
2018-01-10 14:46:36 -0500

    Initial commit of Spring Initializr app; Included components: DevTools, Security, Vaadin, REST Docs, Web, Rest Repositories, Groovy Templates, JPA, Flyway, H2, AMQP, Kafka, Actuator, Actuator Docs

---
+++ src/main/java/org/nationwidechildrens/research/bpci/interact/portal/web/CustomerPortalWebApplication.java
@@ -0,0 +1,12 @@
+package org.nationwidechildrens.research.bpci.interact.portal.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerPortalWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerPortalWebApplication.class, args);
+	}
+}
---
+++ src/test/java/org/nationwidechildrens/research/bpci/interact/portal/web/CustomerPortalWebApplicationTests.java
@@ -0,0 +1,16 @@
+package org.nationwidechildrens.research.bpci.interact.portal.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CustomerPortalWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
